f511b34d53deff5d79c9b62995d1146e6d32eb8f
drojf <drojfjord@gmail.com>
2020-07-29 18:21:15 +1000

    Add some debugging info

--- deploy.py
+++ deploy.py
@@ -42,8 +42,14 @@ def create_umineko_script_zip(url, output_path):
 	# Zip the script
 	zip(f'{temp_folder}/*', output_path)
 
+	print(f"Archive saved to: {os.path.abspath(output_path)}")
+
+
 
 print(f"Start Umineko Question Build: Python {sys.version}")
+print(f"Script running from {os.getcwd()}")
 
 create_umineko_script_zip('https://github.com/07th-mod/umineko-question/raw/master/InDevelopment/ManualUpdates/0.utf', 'output/script-full.7z')
 create_umineko_script_zip('https://github.com/07th-mod/umineko-question/raw/voice_only/InDevelopment/ManualUpdates/0.utf', 'output/script-voice-only.7z')
+
+call(["ls", "-a"])
