8476b676008ce808c9b421d9080f111c94ba2272
drojf <drojfjord@gmail.com>
2020-07-29 18:25:36 +1000

    asdf

--- deploy.py
+++ deploy.py
@@ -52,4 +52,10 @@ print(f"Script running from {os.getcwd()}")
 create_umineko_script_zip('https://github.com/07th-mod/umineko-question/raw/master/InDevelopment/ManualUpdates/0.utf', './output/script-full.7z')
 create_umineko_script_zip('https://github.com/07th-mod/umineko-question/raw/voice_only/InDevelopment/ManualUpdates/0.utf', './output/script-voice-only.7z')
 
-call(["ls", "-a"])
+print("contents of .")
+for file in os.listdir('.'):
+	print(file)
+
+print("contents of output")
+for file in os.listdir('./output'):
+	print(file)
